01b008666ca3912dcb3af2bb56938450b85fc964
KarenLarios <53440589+KarenLarios@users.noreply.github.com>
2019-07-29 13:56:46 -0400

    Add files via upload

--- Karen Larios Day 1 Code .py
+++ Karen Larios Day 1 Code .py
@@ -8,7 +8,27 @@ This is a temporary script file.
 import plotly
 dir(plotly)
 print("My name is Karen")
+5*2
 5/2
 5-2
 5**2
 (8/9)*3
+print(5*2)
+
+def multiply(a,b):
+    multiply = a*b  
+    
+    print(multiply)
+    
+def sum(a,b,c):
+    sum = a+b+c   
+    
+    print(sum)
+sum(3,7,8)
+ 
+def area(a,b,c):
+    area = a*b*c
+    print(5*6*.5)
+    print("the area of the triangle with base of" ,b, " and height of" ,c, " is", area)
+    
+area(.5,6,2)
